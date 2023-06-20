fe666ce9a3dde5d2e5a764e216ca1e849a89b595
aditiksharma <aditiksharma02@berkeley.edu>
2023-06-19 18:02:58 -0700

    Update buyLotsOfFruit.py

--- buyLotsOfFruit.py
+++ buyLotsOfFruit.py
@@ -36,6 +36,12 @@ def buyLotsOfFruit(orderList):
     """
     totalCost = 0.0
     "*** YOUR CODE HERE ***"
+    for fruit, numPounds in orderList:
+        if fruit not in fruitPrices:
+            print (fruit ,"does not exist") 
+            return None
+        costperpound = fruitPrices[fruit]
+        totalCost = costperpound *  numPounds
     return totalCost
 
 
